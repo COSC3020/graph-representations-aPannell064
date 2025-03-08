2762b5f349adf70f358613ed25b3a5002eea7af3
aPannell064 <adamkpannell@gmail.com>
2025-03-08 13:50:13 -0700

    Update code.js

--- code.js
+++ code.js
@@ -5,9 +5,19 @@ function convertToAdjList(adjMatrix) {
         for (var j = 0; j < adjMatrix.length; j++) {
             if (adjMatrix[i][j]) {
                 adjList[i].push(j)
-            }
-            
+            }  
         }
     }
     return adjList;
+}
+
+function convertToAdjMatrix(adjList) {
+    var adjMatrix = []
+    for (var i = 0; i < adjList.length; i++) {
+        adjMatrix.push(new Array(adjList.length).fill(0));
+        for (var j = 0; j < adjList[i].length; j++) {
+            adjMatrix[i][adjList[i][j]] = 1
+        }
+    }
+    return adjMatrix;
 }
